70b91b3eac4e2fc3b565911d1c41f03f69956f4f
APaula-26 <anapauladesouzarodrigues@protonmail.com>
2024-08-09 21:42:52 -0300

    Primeira Pagina WEB

---
+++ Config_Git_Paula.bat
@@ -0,0 +1,12 @@
+echo OFF
+
+echo --- Configurando Usuario GitHub ----
+echo 
+echo 
+git config --global user.name "APaula-26"
+git config --global user.email "anapauladesouzarodrigues@protonmail.com"
+echo --- Mostrando a configuração atual do GitHub no computador 
+git config list  
+echo--- Fim ---
+
+pause
