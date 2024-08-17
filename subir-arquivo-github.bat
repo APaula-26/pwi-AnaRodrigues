752a5c66f2bf3a8bb75a216f06c9123a075f0605
APaula-26 <anapauladesouzarodrigues@protonmail.com>
2024-08-16 22:15:07 -0300

    Meu primeiro site alt 1

--- subir-arquivo-github.bat
+++ subir-arquivo-github.bat
@@ -3,7 +3,7 @@ echo OFF
 echo --- Subir arquivo para o GitHUB ----
 echo
 git add .
-git commit -m "Meu primeiro site alt"
+git commit -m "Meu primeiro site alt 1"
 git push 
 echo
 echo --- Fim ---
