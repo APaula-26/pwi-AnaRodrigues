6c06dd68794643865f2a348ffca0cdd5cca8ad99
Rafael <rafael.ponciano.pereira@gmail.com>
2024-11-29 22:26:23 -0300

    "JS exercicios atualizado"

---
+++ Exercicios_js/exer_17.js
@@ -0,0 +1,10 @@
+//17 - Faça um algoritmo que leia uma temperatura em Fahrenheit e calcule a temperatura correspondente em grau Celsius. Imprima na tela as duas temperaturas.
+// Fórmula: C = (5 * ( F-32) / 9)
+
+let F = 85; // temperatura em fahrenheit
+
+console.log(F,"Temperatura em Fahrenheit");
+
+let resultadoConv = C = (5 * ( F-32) / 9);
+
+console.log(parseInt(resultadoConv),"Temperatura em graus Celsius");
--- subir-arquivo-github.bat
+++ subir-arquivo-github.bat
@@ -3,7 +3,7 @@ echo OFF
 echo --- Subir arquivo para o GitHUB ----
 echo
 git add .
-git commit -m "JS exercicios"
+git commit -m "JS exercicios atualizado"
 git push 
 echo
 echo --- Fim ---
